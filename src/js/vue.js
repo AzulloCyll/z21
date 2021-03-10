b99163d135ff26ec0c7f61bc667c77f77fd43c1b
Daniel Chmur <daniel.chmur@gmail.com>
2021-03-10 18:55:42 +0100

    problems

--- src/js/vue.js
+++ src/js/vue.js
@@ -2,9 +2,28 @@ import Vue from "vue/dist/vue";
 import data from "../json/people.json";
 
 const app = new Vue({
-	el: "#vue",
-	data: {
-		myMessege: "Hello World",
-		json: data.people,
-	},
+  el: "#vue",
+  data: {
+    json: data.people,
+    changeThis: false,
+    button: "Widok Pełny",
+  },
+  methods: {
+    changeView() {
+      if (!this.changeThis) {
+        this.changeThis = true;
+        this.button = "Widok zwinięty";
+      } else {
+        this.changeThis = false;
+        this.button = "Widok Pełny";
+      }
+    },
+    addFriend(event) {
+      // this.json[2].friend = true;
+      console.log(event.target.parentElement);
+      console.log(this.json[1].friend);
+      this.json[1].friend = true;
+      console.log(this.json[1].friend);
+    },
+  },
 });
